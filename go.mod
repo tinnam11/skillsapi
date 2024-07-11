92819c63316844bd25167591b78d4be5a0581fed
Nong AnuchitO <anuchito.code@gmail.com>
2024-07-11 10:53:50 +0700

    [Nong] connect postgres

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gitlab.com/arise-by-infinitas/training/go-database
 
 go 1.22.2
+
+require github.com/lib/pq v1.10.9
--- server.go
+++ server.go
@@ -1,7 +1,14 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	url := os.Getenv("DATABASE_URL")
+
+	fmt.Println("url:", url)
+
+	fmt.Println("done")
 }
