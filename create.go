6602078dd27a85251045f6047bfdd68a30e11819
Nong AnuchitO <anuchito.code@gmail.com>
2024-07-11 10:56:27 +0700

    [Nong] create table

---
+++ create.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+	defer db.Close()
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS todos (
+			id SERIAL PRIMARY KEY,
+			title TEXT,
+			status TEXT
+	);
+	`
+	_, err = db.Exec(createTb)
+
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+
+	fmt.Println("create table success")
+}
